b8db8bf061e2c7f0bb42ba56fc880bd8879e309c
Travis2445 <90297320+Travis2445@users.noreply.github.com>
2021-10-25 17:54:44 -0500

    added playerName var and fight()

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
